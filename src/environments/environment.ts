d7e7faaf01092cd5c6d271ff9d7e502398363e0e
Tanaka <tanakakenne2000@gmail.com>
2023-08-02 10:59:40 +0200

    Add firebase config
    Add firebase and emulators setup

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  useEmulators: true,
+  firebase: {
+    apiKey: "AIzaSyCL50tnUeH77s8xHXyoZ3lA3vQTADk1Wx0",
+    authDomain: "filevisor.firebaseapp.com",
+    projectId: "filevisor",
+    storageBucket: "filevisor.appspot.com",
+    messagingSenderId: "59129126634",
+    appId: "1:59129126634:web:30d28bdec46e6b28278193",
+    measurementId: "G-BGQD1QQ447"
+  }
 };
 
 /*
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,11 @@ import { routes } from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth, connectAuthEmulator } from '@angular/fire/auth';
+import { provideStorage, getStorage, connectStorageEmulator } from '@angular/fire/storage';
+import { connectFirestoreEmulator, getFirestore, provideFirestore } from '@angular/fire/firestore';
+
 if (environment.production) {
   enableProdMode();
 }
@@ -15,6 +20,25 @@ bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
+    importProvidersFrom(
+      provideFirebaseApp(() => initializeApp(environment.firebase)),
+      provideAuth(() => {
+        const auth = getAuth();
+        environment.useEmulators ? connectAuthEmulator(auth, 'http://localhost:9099', { disableWarnings: true }) : '';
+        return auth;
+
+      }),
+      provideFirestore(() => {
+        const firestore = getFirestore();
+        environment.useEmulators ? connectFirestoreEmulator(firestore, 'localhost', 8080) : '';
+        return firestore
+      }),
+      provideStorage(() => {
+        const storage = getStorage();
+        environment.useEmulators ? connectStorageEmulator(storage, 'localhost', 9199) : '';
+        return storage
+      })
+    ),
     provideRouter(routes),
   ],
 });
