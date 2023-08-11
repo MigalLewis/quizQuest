3b62b8a9e4a739004114595ed7ee52696ca4c6aa
Tanaka <tanakakenne2000@gmail.com>
2023-08-11 18:32:56 +0200

    add team card component

---
+++ src/app/components/team-card/team-card.component.spec.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { IonicModule } from '@ionic/angular';
+
+import { TeamCardComponent } from './team-card.component';
+
+describe('TeamCardComponent', () => {
+  let component: TeamCardComponent;
+  let fixture: ComponentFixture<TeamCardComponent>;
+
+  beforeEach(waitForAsync(() => {
+    fixture = TestBed.createComponent(TeamCardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  }));
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
