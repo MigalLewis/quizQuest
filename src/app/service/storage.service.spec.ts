763415d81823a6e374e9818c88c8b780ddcf168f
Tanaka <tanakakenne2000@gmail.com>
2023-08-14 20:31:50 +0200

    Fix failing tests

--- src/app/service/storage.service.spec.ts
+++ src/app/service/storage.service.spec.ts
@@ -1,12 +1,23 @@
 import { TestBed } from '@angular/core/testing';
 
 import { StorageService } from './storage.service';
+import { importProvidersFrom } from '@angular/core';
 
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
+import CordovaSQLiteDriver from 'localforage-cordovasqlitedriver';
 describe('StorageService', () => {
   let service: StorageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        importProvidersFrom(IonicStorageModule.forRoot({
+          name: '__localdb',
+         driverOrder: [CordovaSQLiteDriver._driver, Drivers.IndexedDB, Drivers.LocalStorage]
+        })),
+      ],
+    });
     service = TestBed.inject(StorageService);
   });
 
