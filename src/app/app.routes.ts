b20daf089c38353920c71eff5493577f3eea0cc7
Jose <trocadoj@gmail.com>
2023-09-06 21:57:13 +0200

    [register] Added register page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,17 @@ import { Routes } from '@angular/router';
 import { AuthenticatedPage } from './pages/authenticated/authenticated.page';
 import { HomePage } from './pages/authenticated/home/home.page';
 import { AboutPage } from './pages/authenticated/about/about.page';
+import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
   {
     path: '',
     redirectTo: 'authenticated/home',
     pathMatch: 'full'
+  },{
+    path:'register',
+    component: RegisterComponent,
+    pathMatch: 'full',
   },
   {
     path: 'authenticated',
