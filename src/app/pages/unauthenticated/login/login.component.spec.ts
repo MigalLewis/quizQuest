9196364c9c0fab70aa0267b3f40cd267282346c4
Tanaka <tanakakenne2000@gmail.com>
2023-11-18 18:54:42 +0200

    Implement login

--- src/app/pages/unauthenticated/login/login.component.spec.ts
+++ src/app/pages/unauthenticated/login/login.component.spec.ts
@@ -2,10 +2,18 @@ import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from 'src/app/service/auth.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+
+  beforeEach(async () => {
+    const authServiceSpy = jasmine.createSpyObj('AuthService', ['']);
+    await TestBed.configureTestingModule({
+      providers: [{ provide: AuthService, useValue: authServiceSpy}],
+    }).compileComponents();
+  });
 
   beforeEach(waitForAsync(() => {
     fixture = TestBed.createComponent(LoginComponent);
