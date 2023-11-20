da11e00583741ea32f6fc1c80a199105099b9a38
Tanaka <tanakakenne2000@gmail.com>
2023-11-20 16:23:41 +0200

    Setup automatic redirects

--- src/app/pages/splash/splash.page.ts
+++ src/app/pages/splash/splash.page.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { BackgroundComponent } from 'src/app/components/background/background.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-splash',
@@ -12,10 +13,22 @@ import { BackgroundComponent } from 'src/app/components/background/background.co
   imports: [IonicModule, CommonModule, BackgroundComponent]
 })
 export class SplashPage implements OnInit {
+  timeleft = 3;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.redirect();
+  }
+
+  private redirect() {
+    let downloadTimer = setInterval(() => {
+      if(this.timeleft === 1){
+        clearInterval(downloadTimer);
+        this.router.navigate(['']);
+      }
+      this.timeleft -= 1;
+    }, 1000);
   }
 
 }
