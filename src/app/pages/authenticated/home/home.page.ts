7d0e94f676ae31de982b92e013187bdcecbea3d3
Migal <migallewis@gmail.com>
2024-02-26 22:15:15 +0200

    added session service

--- src/app/pages/authenticated/home/home.page.ts
+++ src/app/pages/authenticated/home/home.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { IonicModule, LoadingController } from '@ionic/angular';
+import { SessionService } from 'src/app/service/session.service';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -10,18 +12,21 @@ import { IonicModule, LoadingController } from '@ionic/angular';
   standalone: true,
   imports: [
     IonicModule, 
-    CommonModule, 
-    FormsModule
+    CommonModule,
+    ReactiveFormsModule
   ]
 })
 export class HomePage implements OnInit {
+  formGroup!: FormGroup;
 
-  constructor(private loadingCtrl: LoadingController) {
+  constructor(private loadingCtrl: LoadingController, 
+    private authService: AuthService,
+    private sessionService: SessionService) {
 
    }
 
   async ngOnInit() {
-
+    this.formGroup = this.createFormGroup();
     
     // const loading = await this.loadingCtrl.create({
     //   message: 'Loading your folders ...'
@@ -29,6 +34,23 @@ export class HomePage implements OnInit {
     // loading.present();
 
     // this.loadingCtrl.dismiss();
+  }
+
+  onJoin() {
+    if(this.formGroup.valid) {
+      let gameCode = this.formGroup.get('gameCode')?.value;
+      let uid = this.authService.getUID();
+      if(uid && gameCode) {
+        this.sessionService.joinSession(gameCode, uid);
+      }
+    }
+
+  }
+
+  createFormGroup(): FormGroup {
+    return new FormGroup({
+      gameCode: new FormControl(undefined, [Validators.required])
+    });
   }
 
 }
