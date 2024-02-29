0de93b274ea68c83c510f93a71806e70204f2488
Migal <migallewis@gmail.com>
2024-02-29 22:33:21 +0200

    :wq

--- src/app/pages/authenticated/pre-game/pre-game.page.ts
+++ src/app/pages/authenticated/pre-game/pre-game.page.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { SessionService } from 'src/app/service/session.service';
-import { QuizSession } from 'src/app/model/quiz.model';
-import { switchMap } from 'rxjs';
+import { QuizSession, SessionStatus } from 'src/app/model/quiz.model';
+import { Subject, switchMap, takeUntil } from 'rxjs';
 import { UserDetail, UserPlayerProfile } from 'src/app/model/user-detail.model';
 
 @Component({
@@ -17,22 +17,31 @@ import { UserDetail, UserPlayerProfile } from 'src/app/model/user-detail.model';
 export class PreGamePage {
   session!: QuizSession;
   users!: UserDetail[];
+  statusIsRunning: Subject<void>;
 
 
-  constructor(private sessionService: SessionService) { }
+  constructor(private sessionService: SessionService) { 
+    this.statusIsRunning = new Subject<void>();
+  }
 
 
   @Input()
   set gameCode(code: string) {
     this.sessionService.getSessionByGameCode(code)
       .pipe(
+        takeUntil(this.statusIsRunning),
         switchMap(session => {
         this.session = session;
         const uids = session.users!;
+        if(this.session.status === SessionStatus.Running) {
+          this.statusIsRunning.next();
+        }
         return this.sessionService.getSessionUsersByUids(uids);
       }))
       .subscribe(users => this.users = users);
   }
+
+  
 
   isChampion(profile: UserPlayerProfile | undefined) {
     if (profile?.numberOfSessionsWon === undefined) {
