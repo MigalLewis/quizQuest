935cba6ad7e69c99221c97a0a3649ffaaa664d6f
Migal <migallewis@gmail.com>
2024-03-02 16:54:37 +0200

    fixed project name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'filevisor',
+  appId: 'biz.pacifish',
+  appName: 'quizQuest',
   webDir: 'www',
   server: {
     androidScheme: 'https'
